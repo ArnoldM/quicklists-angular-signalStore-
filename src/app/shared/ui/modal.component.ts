c15a065a9a16c057d4e91f7a7fa800824c764a39
ArnoldM <arnold.messifet@gmail.com>
2024-01-03 10:27:41 +0100

    Create declarative modal component

---
+++ src/app/shared/ui/modal.component.ts
@@ -0,0 +1,27 @@
+import {
+  Component,
+  ContentChild,
+  Input,
+  TemplateRef,
+  inject,
+} from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+
+@Component({
+  standalone: true,
+  selector: 'app-modal',
+  template: ``,
+})
+export class ModalComponent {
+  dialog = inject(Dialog);
+
+  @ContentChild(TemplateRef, { static: false }) template!: TemplateRef<any>;
+
+  @Input() set isOpen(value: boolean) {
+    if (value) {
+      this.dialog.open(this.template, { panelClass: 'dialog-container' });
+    } else {
+      this.dialog.closeAll();
+    }
+  }
+}
