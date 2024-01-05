824e62b81fcf78bd85a092a91ff0750786323fff
ArnoldM <arnold.messifet@gmail.com>
2024-01-05 09:26:18 +0100

    Create and Use ChecklistService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { ModalComponent } from '../shared/ui/modal.component';
 import { Checklist } from '../shared/interfaces/checklist';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { FormModalComponent } from '../shared/ui/form-modal.component';
+import { ChecklistService } from '../shared/data-access/checklist.service';
 
 @Component({
   standalone: true,
@@ -23,6 +24,7 @@ import { FormModalComponent } from '../shared/ui/form-modal.component';
           "
           [formGroup]="checkListForm"
           (close)="checklistBeingEdited.set(null)"
+          (save)="checklistService.add$.next(checkListForm.getRawValue())"
         />
       </ng-template>
     </app-modal>
@@ -31,6 +33,7 @@ import { FormModalComponent } from '../shared/ui/form-modal.component';
 })
 export default class HomeComponent {
   formBuilder = inject(FormBuilder);
+  checklistService = inject(ChecklistService);
   checklistBeingEdited = signal<Partial<Checklist> | null>(null);
 
   checkListForm = this.formBuilder.nonNullable.group({
--- src/app/shared/interfaces/checklist.ts
+++ src/app/shared/interfaces/checklist.ts
@@ -2,3 +2,7 @@ export interface Checklist {
   id: string;
   title: string;
 }
+
+export type AddChecklist = Omit<Checklist, 'id'>;
+export type EditCheckList = { id: Checklist['id']; data: AddChecklist };
+export type RemoveChecklist = Checklist['id'];
