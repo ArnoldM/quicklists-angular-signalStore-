c4b8a6135b7c70262737285e62155eb402960f71
ArnoldM <arnold.messifet@gmail.com>
2024-01-07 08:02:34 +0100

    Create and  use Cheklist Item and Checklist Item Service

--- src/app/checklist/ui/checklist-header.component.ts
+++ src/app/checklist/ui/checklist-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Checklist } from '../../shared/interfaces/checklist';
 import { RouterLink } from '@angular/router';
 
@@ -9,10 +9,14 @@ import { RouterLink } from '@angular/router';
     <header>
       <a routerLink="/home">Back</a>
       <h1>{{ checklist.title }}</h1>
+      <div>
+        <button (click)="addItem.emit()">Add Item</button>
+      </div>
     </header>
   `,
   imports: [RouterLink],
 })
 export class ChecklistHeaderComponent {
   @Input({ required: true }) checklist!: Checklist;
+  @Output() addItem = new EventEmitter<void>();
 }
